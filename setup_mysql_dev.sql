f0c3caa145b77911dacf2fcea9752b1234b4ff2d
misaelbazan <misaelbfranco@gmail.com>
2024-01-06 22:58:16 -0500

    sql_ file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- Creating an script that prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'; 
 GRANT SELECT ON performance_scheme.* TO 'hbnb_dev'@'localhost';
