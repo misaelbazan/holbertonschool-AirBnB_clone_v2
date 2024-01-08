f010df48543e77431658e604b71a74fad80e15e7
misaelbazan <misaelbfranco@gmail.com>
2024-01-07 21:59:58 -0500

    3rd task files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,15 @@
 -- Creating an script that prepares a MySQL server for the projecta
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'; 
-GRANT SELECT ON `performance_scheme`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER 
+  IF NOT EXISTS 'hbnb_dev'@'localhost' 
+  IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES 
+  ON `hbnb_dev_db`.* 
+  TO 'hbnb_dev'@'localhost' 
+  IDENTIFIED BY 'hbnb_dev_pwd'; 
+GRANT SELECT 
+  ON `performance_scheme`.* 
+  TO 'hbnb_dev'@'localhost' 
+  IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
