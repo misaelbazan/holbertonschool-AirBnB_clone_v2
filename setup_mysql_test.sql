fe4b2df113d78722a90f023f2fd42fac2364b323
misaelbazan <misaelbfranco@gmail.com>
2024-01-07 22:24:56 -0500

    4th task files

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- Creating a script that prepares a MySQL server for the project
+-- a db 'hbnb_test_db'
+-- a new user ' hbnb_test' in localhost
+-- the password of 'hbnb_test' should be 'hbnb_test_pwd'
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
