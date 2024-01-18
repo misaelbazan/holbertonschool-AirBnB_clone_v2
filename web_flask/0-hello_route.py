32675ee1b8e59764282fde8879e88d2a88360c21
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-18 00:06:00 -0500

    update flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB"
+    return "<p>Hello HBNB!</p>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000)
