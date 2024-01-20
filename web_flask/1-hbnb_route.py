c272d6c7f8b295aae5bfcdf873282cc99eff2caf
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-20 15:49:19 -0500

    using flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def hello_hbnb():
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    return "HBNB"
+
 # Check if this script is the main program
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
