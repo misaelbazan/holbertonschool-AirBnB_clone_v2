c34a73e2cfa438aa31aea4d7510c8140a7361565
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-20 13:46:04 -0500

    updarte

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Script that starts a Flask web application"""
 from flask import Flask
 
 
+# Create an instance of the Flask application
 app = Flask(__name__)
 
 
+# Create an instance of the Flask application
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     return "Hello, World!"
 
+# Check if this script is the main program
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
