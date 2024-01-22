b0f5248fd59aa61346b04b449fe2dbee6954c69f
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-21 19:28:19 -0500

    using render_template to link to an html page

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -11,17 +11,22 @@ app = Flask(__name__)
 # Route for the main page
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """this script will return 'hello HBNB'"""
     return "Hello HBNB!"
 
 # Route for "/hbnb"
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """
+    It will return the content only 
+    if it is in the hbnb url.
+    """
     return "HBNB"
 
 # Route for "/c/<text>"
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
-    # Replace underscores with spaces in the text variable
+    """this script accepts only text."""
     text = escape(text).replace('_', ' ')
     return "C {}".format(text)
 
@@ -29,16 +34,23 @@ def c_text(text):
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is_cool"):
+    """
+    This script accepts only text if it does
+    not display the default value.
+    """
     text = escape(text).replace('_', ' ')
     return "python {}".format(text)
 
 # Route for "/number/<int:n>"
 @app.route("/number/<int:n>", strict_slashes=False)
 def num_int(n):
+    """This script accepts only integers."""
     return "{} is a number".format(n)
 
+# Route for "/number_template/<int:n>"
 @app.route("/number_template/<int:n>")
 def html_int(n):
+    """This script links to an html page."""
     return render_template("5-number.html", number=n)
 
 
