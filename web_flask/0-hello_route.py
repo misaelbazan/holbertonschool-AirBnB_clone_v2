e840ab4ddcd7f46fef0cb1dd8cc8350bde569d11
misaelbazan <misaelbfranco@gmail.com>
2024-01-23 18:39:01 -0800

    PEP8 validations passed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,13 @@ from flask import Flask
 """Create an instance of the Flask application"""
 app = Flask(__name__)
 
+
 # Route from the main page
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-  return "Hello HBNB!"
+    return "Hello HBNB!"
+
 
 # Check if this script is the main program
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
