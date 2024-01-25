6381d7c798eef166f1534101d1f707989dc84319
misaelbazan <misaelbfranco@gmail.com>
2024-01-24 17:10:35 -0800

    4th task file

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application
+    Must be listening to 0.0.0.0:5000
+    Routes:
+        - (/) : display "Hello HBNB!"
+        - (/hbnb) : display "HBNB"
+        - (/c/<text>) : display "C" followed by the value of the <text>
+        variable (Replace "_" with a space " ")
+    .You must use the option strict_slashes=False in the route definition
+"""
+
+from flask import Flask
+
+
+# Create an instance of the Flask application
+app = Flask(__name__)
+
+
+# Route from the main page
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+# Route from /hbnb page
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+# Route from "/c/<text>"
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    # Use replace method for replace "_" with spaces " "
+    result = text.replace("_", " ")
+    return f"C {result}"
+
+
+# Route from "/python/<text>, with 'is cool' as default value"
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    # Replacing underscores "_" with spaces " "
+    result = text.replace("_", " ")
+    return f"Python {result}"
+
+
+# Route from "/number/<n>, to display a text only if <n> is an int.
+@app.route("/number/<n>", strict_slashes=False)
+def n_is_int(n):
+    # Validationg if "n" is int
+    try:
+        n = int(n)
+        return f"{n} is a number"
+    except ValueError:
+        pass
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
